b9a8340fbd476f92476e9d007e8eb0d483d43b64
zakdave <74794944+zakdave@users.noreply.github.com>
2021-04-12 12:35:54 -0400

    connection to db

--- client/src/Data.js
+++ client/src/Data.js
@@ -23,3 +23,94 @@ export default class Data {
 
         return fetch(url, options);
     }
+
+    async getUser(emailAddress, password) {
+        const response = await this.api('/users', 'GET', null, true, {
+            emailAddress,
+            password
+        });
+
+        if (response.status === 200) {
+            return response.json().then(data => data);
+        }
+        else if (response.status === 401) {
+            return null;
+        }
+        else {
+            throw new Error();
+        }
+    }
+
+    async createUser(user) {
+        const response = await this.api('/users', 'POST', user);
+        if (response.status === 201) {
+            return [];
+        }
+        else if (response.status === 400) {
+            return response.json().then(data => {
+                return data.errors;
+            });
+        }
+        else {
+            throw new Error();
+        }
+    }
+
+    async createCourse(course, emailAddress, password) {
+        const response = await this.api('/courses', 'POST', course, true, {
+            emailAddress,
+            password
+        });
+
+
+        if (response.status === 201) {
+            return [];
+        }
+        else if (response.status === 400) {
+            return response.json().then(data => {
+                return data.errors;
+            });
+        }
+        else {
+            throw new Error();
+        }
+    }
+ 
+    async updateCourse(emailAddress, password, id, course) {
+        const response = await this.api(`/courses/${id}`, 'PUT', course, true, {
+            emailAddress,
+            password
+        });
+
+        if (response.status === 204) {
+            return [];
+        }
+        else if (response.status === 400) {
+            return response.json().then(data => {
+                return data.errors;
+            });
+        }
+        else {
+            throw new Error();
+        }
+
+    }
+    
+    async deleteCourse(id, emailAddress, password) {
+        const response = await this.api(`/courses/${id}`, 'DELETE', null, true, {
+            emailAddress,
+            password
+        })
+        if (response.status === 204) {
+            return [];
+        }
+        else if (response.status === 403) {
+            return response.json().then(data => {
+                return data.errors;
+            });
+        }
+        else {
+            throw new Error();
+        }
+    }
+}
